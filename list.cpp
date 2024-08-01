1f316113801a1aad4a5297072593c46de3128a07
kavan_vaghela <131892792+kvn05@users.noreply.github.com>
2024-08-01 21:19:00 +0530

    Add list.cpp

---
+++ list.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    void explainList()
+    {
+        list<int> ls;
+
+        ls.push_back(2);    // {2}
+        ls.emplace_back(4); // {2,4}
+
+        ls.push_front(5); // {5,2,4}
+
+        ls.emplace_front(); // {2,4}
+    }
+}
